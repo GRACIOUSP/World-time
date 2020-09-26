070df02c7bebdfe52e877223912c9323657f35bd
Demartechx <adeagbothompson@gmail.com>
2020-09-26 01:19:19 +0100

    World time app with flutter

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  @override
+  _ChooseLocationState createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+  void getData() async {
+    // simulate network request for a username
+    String username = await Future.delayed(Duration(seconds: 3), () {
+      return 'yoshi';
+    });
+
+    // simulate network request to get bio of the username
+    String bio = await Future.delayed(Duration(seconds: 2), () {
+      return 'vega, musician & egg collector';
+    });
+
+    print('$username - $bio');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a Location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: RaisedButton(
+        onPressed: () {
+          setState(() {
+            //counter++;
+          });
+        },
+        //child: Text('counter is $counter'),
+      ),
+    );
+  }
+}
